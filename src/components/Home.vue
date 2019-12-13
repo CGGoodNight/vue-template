0bf3b2a42aa713606e6c5a3ad1b722017ebd915a
TwistChen <565420809@qq.com>
2019-12-13 14:50:14 +0800

    add less

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <h3 class='less'><span>Less</span></h3>
+    <span class="import-less">外部引入less</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang='less'>
+  @import './index.less';
+  .less {
+    span {
+      font-size: 22px;
+      font-weight: 700;
+      color: #f10215;
+    }
+  }
+</style>
